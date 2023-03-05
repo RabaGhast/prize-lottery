15a7ed36b110b4f9ad99b314c6ce59ce0d901b95
Fabian Boe <fabianboe@hotmail.com>
2023-03-05 14:51:03 +0100

    Web API: Removed example code and added dummy Ticket API

---
+++ src/Web-API/Controllers/TicketController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_API.Models;
+
+namespace Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private static List<Ticket> DummyData = new List<Ticket>
+        {
+            new Ticket // Not reserved
+            {
+                Number = 1,
+                ReservedBy = null,
+                IsDrawn = false,
+                IsPaid = false
+            },
+            new Ticket // Reserverd
+            {
+                Number = 2,
+                ReservedBy = "Fabian",
+                IsDrawn = true,
+                IsPaid = false
+            },
+            new Ticket // Reserved and paid
+            {
+                Number = 3,
+                ReservedBy = "Fabian",
+                IsDrawn = true,
+                IsPaid = true
+            }
+        };
+
+        // GET: api/<TicketController>
+        [HttpGet]
+        public IEnumerable<Ticket> Get()
+        {
+            return DummyData;
+        }
+
+        // GET api/<TicketController>/5
+        [HttpGet("{id}")]
+        public Ticket? Get(int id)
+        {
+            return DummyData.FirstOrDefault(t => t.Number == id);
+        }
+
+        // POST api/<TicketController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TicketController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TicketController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/Web-API/Models/Ticket.cs
@@ -0,0 +1,28 @@
+﻿namespace Web_API.Models
+{
+    /// <summary>
+    /// Represents a ticket for a lottery or raffle.
+    /// </summary>
+    public class Ticket
+    {
+        /// <summary>
+        /// The number on the ticket, used to uniquely identify it.
+        /// </summary>
+        public int Number { get; set; }
+
+        /// <summary>
+        /// The name of the person who reserved the ticket.
+        /// </summary>
+        public string? ReservedBy { get; set; } = null;
+
+        /// <summary>
+        ///  A value indicating whether the ticket has been paid for.
+        /// </summary>
+        public bool IsPaid { get; set; } = false;
+
+        /// <summary>
+        ///  A value indicating whether the ticket has been drawn as a winner.
+        /// </summary>
+        public bool IsDrawn { get; set; } = false;
+    }
+}
