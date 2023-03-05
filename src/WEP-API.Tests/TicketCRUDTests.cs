9876049d0ce380781e98a7531dcf09af8d9b2174
Fabian Boe <fabianboe@hotmail.com>
2023-03-05 18:47:44 +0100

    Web API: Added ticket case tests. Restructured tickets CRUD tests

--- src/WEP-API.Tests/TicketCRUDTests.cs
+++ src/WEP-API.Tests/TicketCRUDTests.cs
@@ -12,11 +12,10 @@ public class TicketCRUDTests
     HttpClient _client;
 
     private const string TicketEndpoint = "api/Ticket";
-
     private static Ticket ExampleTicket = new Ticket
     {
-        ReservedBy = "John Doe",
-        IsPaid = true,
+        ReservedBy = null,
+        IsPaid = false,
         IsDrawn = false
     };
 
@@ -35,56 +34,62 @@ public class TicketCRUDTests
     [Test]
     public async Task AddTicket()
     {
+        // Add dummy ticket
         var ticket = await AddTicketRequest();
-        Assert.NotNull(ticket);
-        Assert.AreEqual(ExampleTicket.ReservedBy, ticket.ReservedBy);
-        Assert.AreEqual(ExampleTicket.IsPaid, ticket.IsPaid);
-        Assert.AreEqual(ExampleTicket.IsDrawn, ticket.IsDrawn);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(ticket, Is.Not.Null);
+            Assert.That(ticket.ReservedBy, Is.EqualTo(ExampleTicket.ReservedBy));
+            Assert.That(ticket.IsPaid, Is.EqualTo(ExampleTicket.IsPaid));
+            Assert.That(ticket.IsDrawn, Is.EqualTo(ExampleTicket.IsDrawn));
+        });
     }
 
     [Test]
     public async Task GetTicket()
     {
         var tickets = await _client.GetFromJsonAsync<List<Ticket>>($"{TicketEndpoint}/1");
-        Assert.NotNull(tickets);
+        Assert.That(tickets, Is.Not.Null);
     }
 
     [Test]
     public async Task GetAllTickets()
     {
         var tickets = await _client.GetFromJsonAsync<List<Ticket>>(TicketEndpoint);
-        Assert.NotNull(tickets);
+        Assert.That(tickets, Is.Not.Null);
     }
 
 
     [Test]
     public async Task UpdateTicket()
     {
-
+        // Add dummy ticket
         var ticket = await AddTicketRequest();
 
-        // Update the ticket using its ID
+        // Update the dummy ticket using its ID
         ticket.IsDrawn = true;
         var content = new StringContent(JsonSerializer.Serialize(ticket), Encoding.UTF8, "application/json");
         var putResponse = await _client.PutAsync($"{TicketEndpoint}/{ticket!.Number}", content);
         putResponse.EnsureSuccessStatusCode();
 
         var ticketFromPutResponse = await putResponse.Content.ReadFromJsonAsync<Ticket>();
-        Assert.AreEqual(ticket, ticketFromPutResponse);
+        Assert.That(ticketFromPutResponse, Is.EqualTo(ticket));
     }
 
     [Test]
     public async Task DeleteTicket()
     {
+        // Add dummy ticket
         var ticket = await AddTicketRequest();
 
-        // Delete the ticket using its ID
+        // Delete the dummy ticket using its ID
         var deleteResponse = await _client.DeleteAsync($"{TicketEndpoint}/{ticket!.Number}");
         deleteResponse.EnsureSuccessStatusCode();
 
-        // Ensure that the ticket was deleted by trying to retrieve it again
+        // Ensure that the dummy ticket was deleted by trying to retrieve it again
         var getResponse = await _client.GetAsync($"{TicketEndpoint}/{ticket.Number}");
-        Assert.AreEqual(HttpStatusCode.NotFound, getResponse.StatusCode);
+        Assert.That(getResponse.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
     }
 
     private async Task<Ticket> AddTicketRequest()
---
+++ src/WEP-API.Tests/TicketUserCaseTests.cs
@@ -0,0 +1,138 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
+using Web_API.Models;
+
+namespace WEP_API.Tests;
+
+public class TicketUserCaseTests
+{
+    HttpClient _client;
+
+    private const string TicketEndpoint = "api/Ticket";
+    private const string User = "Fabian";
+    private const int numberOfTickets = 100;
+
+    [SetUp]
+    public void Setup()
+    {
+        var application = new WebApplicationFactory<Program>();
+        _client = application.CreateClient();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _client.Dispose();
+    }
+
+    [Test]
+    public async Task ReserveTicketBasedOnUsername()
+    {
+        // Create dummy ticket for test
+        var ticket = await AddDummyRequest(new Ticket
+        {
+            ReservedBy = null,
+            IsPaid = false,
+            IsDrawn = false
+        });
+
+        // Reserve dummy ticket
+        var response = await _client.PostAsync($"{TicketEndpoint}/{ticket!.Number}/reserve?user={User}", null);
+        response.EnsureSuccessStatusCode();
+
+        // Make sure ticket is marked as reserved
+        var ticketFromPutResponse = await response.Content.ReadFromJsonAsync<Ticket>();
+        Assert.That(ticketFromPutResponse.ReservedBy, Is.EqualTo(User));
+    }
+
+    [Test]
+    public async Task PayTicketsBasedOnUsername()
+    {
+        // Create dummy tickets for test
+        var tickets = await Task.WhenAll(Enumerable.Range(1, 5)
+            .Select(async i => await AddDummyRequest(new Ticket
+            {
+                ReservedBy = null,
+                IsPaid = false,
+                IsDrawn = false
+            })));
+
+        // Reserve all dummy tickets
+        foreach (var ticket in tickets)
+        {
+            var response = await _client.PostAsync($"{TicketEndpoint}/{ticket!.Number}/reserve?user={User}", null);
+            response.EnsureSuccessStatusCode();
+        }
+
+        // Pay for tickets by username
+        var payResponse = await _client.PostAsync($"{TicketEndpoint}/pay?user={User}", null);
+        payResponse.EnsureSuccessStatusCode();
+
+        // Ensure that all tickets are marked as payed
+        var allTicketsResponse = await _client.GetAsync(TicketEndpoint);
+        var allTickets = await allTicketsResponse.Content.ReadFromJsonAsync<List<Ticket>>();
+
+        foreach (var ticket in tickets.Where(t => t.ReservedBy.Equals(User)))
+        {
+            Assert.That(ticket.IsPaid);
+        }
+    }
+
+    [Test]
+    public async Task InitializeTickets()
+    {
+        var response = await _client.PostAsync($"{TicketEndpoint}/initialize?numberOfTickets={numberOfTickets}", null);
+        response.EnsureSuccessStatusCode();
+
+        var ticketsFromResponse = await response.Content.ReadFromJsonAsync<List<Ticket>>();
+        Assert.That(ticketsFromResponse, Is.Not.Null);
+        Assert.That(ticketsFromResponse.Count, Is.EqualTo(numberOfTickets));
+
+        foreach (var ticket in ticketsFromResponse)
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(ticket.ReservedBy, Is.Null);
+                Assert.That(ticket.IsDrawn, Is.False);
+                Assert.That(ticket.IsPaid, Is.False);
+            });
+        }
+    }
+
+    [Test]
+    public async Task DrawRandom()
+    {
+        // Create dummy tickets for test
+        var ticketsBeforeDraw = await Task.WhenAll(Enumerable.Range(1, 5)
+            .Select(async i => await AddDummyRequest(new Ticket
+        {
+            ReservedBy = User,
+            IsPaid = true,
+            IsDrawn = i % 2 == 0
+        })));
+
+        // Draw ticket
+        var drawnTicket = await _client.GetFromJsonAsync<Ticket>($"{TicketEndpoint}/draw");
+        var drawnTicketBeforeDraw = ticketsBeforeDraw.FirstOrDefault(t => t.Number.Equals(drawnTicket.Number));
+
+        // Ensure that the drawn ticket has been updated corretly
+        Assert.Multiple(() =>
+        {
+            Assert.That(drawnTicketBeforeDraw, Is.Not.Null);
+            Assert.That(drawnTicket, Is.Not.Null);
+            Assert.That(drawnTicket.ReservedBy, Is.Not.Null);
+            Assert.That(drawnTicket.IsPaid, Is.True);
+            Assert.That(drawnTicket.IsDrawn, Is.True);
+        });
+    }
+
+    private async Task<Ticket> AddDummyRequest(Ticket ticket)
+    {
+        var response = await _client.PostAsJsonAsync(TicketEndpoint, ticket);
+        response.EnsureSuccessStatusCode();
+
+        var ticketFromResponse = await response.Content.ReadFromJsonAsync<Ticket>();
+        return ticketFromResponse;
+    }
+
+}
